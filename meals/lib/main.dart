4e2e09a01eb17380e3995ada5890607b1733bb4b
CaioNoboa <caiosalvador@hotmail.com>
2022-07-07 14:07:06 -0300

    appRoutes

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:meals/screens/meal_detail_screen.dart';
 import 'package:meals/utils/app_routes.dart';
 import 'package:meals/utils/theme.dart';
-import 'package:meals/screens/categories_meals_screen.dart';
 import 'package:meals/screens/categories_screen.dart';
 
 void main() {
@@ -17,10 +15,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Vamos cozinhar?',
       theme: appTheme,
-      routes: {
-        AppRoutes.home: (context) => const CategoriesScreen(), // Rota inicial
-        AppRoutes.categoriesMeals: (context) => const CategoriesMealsScreen(),
-        AppRoutes.mealDetail: (context) => const MealDetailScreen(),
+      routes: appRoutes,
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (_) {
+          return const CategoriesScreen();
+        });
       },
     );
   }
--- meals/lib/utils/app_routes.dart
+++ meals/lib/utils/app_routes.dart
@@ -1,5 +1,15 @@
+import '../screens/categories_meals_screen.dart';
+import '../screens/categories_screen.dart';
+import '../screens/meal_detail_screen.dart';
+
 class AppRoutes {
   static const home = '/';
   static const categoriesMeals = '/categories-meals';
   static const mealDetail = '/meal-detail';
 }
+
+final appRoutes = {
+  AppRoutes.home: (context) => const CategoriesScreen(), // Rota inicial
+  AppRoutes.categoriesMeals: (context) => const CategoriesMealsScreen(),
+  AppRoutes.mealDetail: (context) => const MealDetailScreen(),
+};
